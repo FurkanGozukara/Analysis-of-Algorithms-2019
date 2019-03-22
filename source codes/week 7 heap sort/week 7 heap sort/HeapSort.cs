f88688d251ded10419925a91ad97305bbf51a188
Furkan Gözükara <furkangozukara@gmail.com>
2019-03-22 11:35:10 +0300

    week 7

---
+++ source codes/week 7 heap sort/week 7 heap sort/HeapSort.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_7_heap_sort
+{
+    class HeapSort
+    {
+        private int heapSize;
+
+        private void BuildHeap(int[] arr)
+        {
+            heapSize = arr.Length - 1;
+            for (int i = heapSize / 2; i >= 0; i--)
+            {
+                Console.WriteLine("calling max Heapify i=" + i);
+                Heapify(arr, i);
+            }
+        }
+
+        private void Swap(int[] arr, int x, int y)//function to swap elements
+        {
+            int temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+        private void Heapify(int[] arr, int index)
+        {
+            Console.WriteLine("starting max heapify index " + index + " array: " + string.Join(", ", arr));
+            int left = 2 * index;
+            int right = 2 * index + 1;
+            int largest = index;
+
+            if (left <= heapSize && arr[left] > arr[index])
+            {
+                largest = left;
+            }
+
+            if (right <= heapSize && arr[right] > arr[largest])
+            {
+                largest = right;
+            }
+
+            if (largest != index)
+            {
+                Swap(arr, index, largest);
+                Heapify(arr, largest);
+            }
+        }
+        public void PerformHeapSort(int[] arr)
+        {
+            Console.WriteLine("calling build max heap");
+            BuildHeap(arr);
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Swap(arr, 0, i);
+                heapSize--;
+                Heapify(arr, 0);
+            }
+            DisplayArray(arr);
+        }
+        private void DisplayArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            { Console.Write("[{0}]", arr[i]); }
+        }
+    }
+}
---
+++ source codes/week 7 heap sort/week 7 heap sort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_7_heap_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 64, 7, 52, 32, 18, 2, 48 };
+
+            var tempArray = arr.ToArray();//this composes a new array thus original list is not sorted
+
+            var temparray2 = arr;//this assign reference thus original list get sorted
+
+            HeapSort hs = new HeapSort();
+
+            hs.PerformHeapSort(tempArray);
+
+         //   hs.PerformHeapSort(temparray2);//this sorts original array too
+
+            Console.ReadLine();
+        }
+    }
+}
